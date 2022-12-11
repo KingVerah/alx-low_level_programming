5443b659170364e14894f46dc43e0bd5f5d56885
Kudakwashe <kudaverah@gmail.com>
2022-12-11 10:43:04 -0800

    hello world directory

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
-/**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
+#include <stdio.h>
+#include <stdio.h>/**
+#include <stdio.h>* main - print the string in the put function
+#include <stdio.h>*
+#include <stdio.h>* Description: using the main function
+#include <stdio.h>* this program prints "with proper grammar, but the outcome is a piece of art
 * Return: 0
-*/
+* */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
