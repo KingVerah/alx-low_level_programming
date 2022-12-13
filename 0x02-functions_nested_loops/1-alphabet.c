f94db5217903b4b3ef0683e7bcec4448bbbe518b
Kudakwashe <kudaverah@gmail.com>
2022-12-13 08:05:22 -0800

    Project 0x02

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+Â#include "main.h"
+
+/**
+* main - prints the alphabet in lower case followed by a new line
+*
+* Return: void
+*/
+void main(void)
+{
+	char i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
