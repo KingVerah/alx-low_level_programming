d8c9760e5feaa2976908a565ac5882d188486fdd
Kudakwashe <kudaverah@gmail.com>
2022-12-13 08:18:02 -0800

    Project 0x02

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,12 @@
 * followed by a new line
 * Return: void
 */
+
 void print_alphabet_x10(void)
 {
 	char letter;
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
