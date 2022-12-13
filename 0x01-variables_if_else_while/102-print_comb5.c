e8cf70147b5a3c5d4ec77ac4649201eb69c5fc26
Kudakwashe <kudaverah@gmail.com>
2022-12-12 17:11:47 -0800

    project 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,24 +10,25 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; j <= 98; i++)
+	for (i = 0; j <= 100; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-
-			if (i == 98 && j == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+			if (i <j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
 	return (0);
-
 }
