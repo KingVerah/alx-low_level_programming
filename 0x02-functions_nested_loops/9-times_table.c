e549eeca6c523dd3652b6fa0f2af3fa75a755a37
Kudakwashe <kudaverah@gmail.com>
2022-12-13 09:11:02 -0800

    Project 0x02

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,11 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0');
 
-		for (j = 1; j <=9; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
