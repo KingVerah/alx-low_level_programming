42efb1662608bc6b02c07bf15f63342049b2dc57
Kudakwashe <kudaverah@gmail.com>
2022-12-15 15:37:29 -0800

    Project 5

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,11 +10,17 @@ int main(void)
 {
 	long int p, i = 612852475143;
 	
-	for (p = 2; p < i; p++)
+	while (p++ < i / 2)
 	{
 		if (i % p == 0)
 		{
-			i = i / p;
+			i /= 2;
+			continue;
+		}
+		for (p = 3; p < i / 2; p += 2)
+		{
+			if (i % p == 0)
+				i /= p;
 		}
 	}
 	printf("%ld\n", p);
