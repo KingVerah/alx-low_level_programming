fbd4201bf0dd9ec313cdc131f9d6b064aea2e51c
Kudakwashe <kudaverah@gmail.com>
2022-12-15 13:47:46 -0800

    Project 4

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
 /**
-* main - causes an infine loop
-* Return: 0
-*/
+ * main - causes an infine loop
+ * Return: 0
+ */
 
 int main(void)
 {
-int i;
-
-printf("Infinite loop incoming :(\n");
-
-i = 0;
-/*
-* while (i < 10)
-* {
-* putchar(i);
-* }
-*/
-printf("Infinite loop avoided \n");
-
-return (0);
+	int i;
+	
+	printf("Infinite loop incoming :(\n");
+	
+	i = 0;
+	
+	/**
+	 * while (i < 10)
+	 * {
+	 *	 putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+	
+	return (0);
 }
