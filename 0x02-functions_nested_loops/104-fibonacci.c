055f89ab79d4f24ba88335d14dd6f853fd348a2a
Kudakwashe <kudaverah@gmail.com>
2022-12-15 16:58:06 -0800

    update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,28 @@
-#include "main.h"                                                                                                                     
-#include <stdio.h>                                                                                                                    
+#include "main.h"
+#include <stdio.h>
 
-/**                                                                                                                                   
+/**
  * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
  * followed by a new line
  * Return: Always 0
  */
 
-int main(void)
+int main
 {
-	int c;
-	unsigned long i = 0, j = 1, sum;
-	
-	for (c = 0; c < 98; c++)
-	{
-		sum = i + j;
-		printf("%lu", sum);
-		
-		i = j;
-		j = sum;
-		
-		if (c == 97)
-			printf("\n");
-		else
-		{
-			printf(", ");
-		}
+	int i, n;
+	int f1 = 0, f2 = 1;
+
+	int t = f1 + f2;
+
+	n = 98;
+	printf("%d, %d ", f1, f2);
+
+	for (i = 3; i <= n; i++)
+	{	
+		printf("%d, ", t);
+		f1 = f2;
+		f2 = t;
+		t = f1 + f2;
 	}
 	return (0);
 }
