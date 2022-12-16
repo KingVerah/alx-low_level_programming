8edcea8dfc6034cd10e6d709c0ad0b8030c6633c
Kudakwashe <kudaverah@gmail.com>
2022-12-15 17:43:24 -0800

    update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,13 +11,12 @@ int main(void)
 {
 	int f1 = 0, f2 = 1, f3, i, n = 98;
 
-	while (i < n)
+	for (i = 2; i < n; ++i)
 	{
 		f3 = f1 + f2;
-		printf("%d, ", f3);
+		printf(" %d", f3);
 		f1 = f2;
 		f2 = f3;
-		i++;
 	}
 	return (0);
 }
