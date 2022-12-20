b7f59f57adb8619c32572db62af72c826e46831b
Kudakwashe <kudaverah@gmail.com>
2022-12-19 23:26:30 -0800

    Update 0x05-pointers_arrays_strings project

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string
+ * @str: string to print
+ *
+ * On sucess: Returns no error
+ */
+
+void _pus(char *str)
+{
+	int i = o;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
