0c9553e35bdf5ef84d49c87e605ea600bdb5ea02
Kudakwashe <kudaverah@gmail.com>
2022-12-19 23:41:26 -0800

    Update 0x05-pointers_arrays_strings project

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: String to be reserved
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int i, tmp, len = _strlen(s);
+
+	int i, tmp, len = _strlen(s);
+	{
+		tmp = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
+	}
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String
+ * Return: The length of the given string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
